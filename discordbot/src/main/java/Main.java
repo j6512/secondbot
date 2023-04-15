041f0626e7341671884b2cb886e64ee029114f34
j6512 <justins8287@gmail.com>
2023-04-15 07:26:53 -0500

    Events and token environment variable

--- discordbot/src/main/java/Main.java
+++ discordbot/src/main/java/Main.java
@@ -1,13 +1,20 @@
+import events.MessageEventListener;
+import events.ReadyEventListener;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        final String TOKEN = "MTA5NjczNjE2OTU2NTM2ODQwMQ.G-DWvj.G3o3f3D9n0bA45iRHNhLch-EIksfzlL_V1MUvs";
+        final String TOKEN = System.getenv("SECOND_BOT_TOKEN");
+
         JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN);
-        jdaBuilder.setActivity(Activity.playing("Test Activity"));
-        jdaBuilder.build();
+        jdaBuilder
+                .setActivity(Activity.playing("Test Activity"))
+                .addEventListeners(new ReadyEventListener(), new MessageEventListener())
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .build();
     }
 }
