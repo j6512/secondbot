124c42a299d6c6d0dab57e28ac24dac3a06c5d0d
j6512 <justins8287@gmail.com>
2023-04-16 04:54:36 -0500

    Slash command event listener

--- discordbot/src/main/java/Main.java
+++ discordbot/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import events.MessageEventListener;
 import events.ReadyEventListener;
+import events.SlashCommandInteractionEventListener;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -11,10 +13,17 @@ public class Main {
         final String TOKEN = System.getenv("SECOND_BOT_TOKEN");
 
         JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN);
-        jdaBuilder
+        JDA jda = jdaBuilder
                 .setActivity(Activity.playing("Test Activity"))
-                .addEventListeners(new ReadyEventListener(), new MessageEventListener())
+                .addEventListeners(
+                        new ReadyEventListener(),
+                        new MessageEventListener(),
+                        new SlashCommandInteractionEventListener())
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .build();
+
+        jda.upsertCommand("slash-cmd", "This is a slash command").setGuildOnly(true).queue();
+        jda.upsertCommand("flash-cards", "Lists all flash cards").setGuildOnly(true).queue();
+        jda.upsertCommand("quiz", "Gives you a random quizzzz").setGuildOnly(false).queue();
     }
 }
